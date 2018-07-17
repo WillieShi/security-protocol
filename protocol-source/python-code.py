7fb953500fd29d8985dd62c36f413836ac58ec85
Allan Tenenbaum <tenenbaumallan@gmail.com>
2018-07-17 15:43:55 -0400

    managed to work out AES

--- protocol-source/python-code.py
+++ protocol-source/python-code.py
@@ -3,7 +3,6 @@ from Crypto import Random
 from Crypto.Cipher import AES
 import base64
 
-from Crypto.Cipher import AES
 # Encryption
 #encryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
 #cipher_text = encryption_suite.encrypt("A really secret message. Not for prying eyes.")
@@ -11,18 +10,17 @@ from Crypto.Cipher import AES
 # Decryption
 #decryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
 #plain_text = decryption_suite.decrypt(cipher_text)
-encryption_key = "This is a key123"
-plain_text_message = "A really secret message. Not for prying eyes."
 
-def encrypt(message, key):
+encryption_key = "This is a key123"
+plain_text_message = "aaaaaaaaaaaaaaaa"
+
+def encrypt_message(message, key):
     encryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
+    print(len(message))
     cipher_text = encryption_suite.encrypt(message)
     return cipher_text
 
-def decrypt(message, key):
-    ecryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
+def decrypt_message(message, key):
+    decryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
     plain_text = decryption_suite.decrypt(message)
-
-encoded_text = encrypt(plain_text_message, encryption_key)
-
-print(encoded_text)
+    return plain_text
