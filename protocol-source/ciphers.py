4a461c0ecd5d7eb47f13765fb7b3ddb5d17b3508
Allan Tenenbaum <tenenbaumallan@gmail.com>
2018-07-17 16:58:25 -0400

    generate large primes easily but not sure if actually prime

--- protocol-source/ciphers.py
+++ protocol-source/ciphers.py
@@ -1,29 +1,48 @@
 from Crypto import Random
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Hash import SHA
+from Crypto.Util import number
+import os
 import base64
+import six
 
-# Encryption
-#encryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-#cipher_text = encryption_suite.encrypt("A really secret message. Not for prying eyes.")
-
-# Decryption
-#decryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-#plain_text = decryption_suite.decrypt(cipher_text)
+#here we define variables
+n_length = 256
+prime_number_1 = 0
+prime_number_2 = 0
 
 #both the key and the message must be a multiple of 16 in length"
-aes_key = "This is a key123"
-plain_text_message = "aaaaaaaaaaaaaaaa"
 
-def encrypt_message(message, key):
+def encrypt_message_AES(message, key):
     #don't know what this line does. the var key is passed as an argument
     encryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
     #cipher_text is the encrypted code
     cipher_text = encryption_suite.encrypt(message)
     return cipher_text
 
-def decrypt_message(message, key):
+def decrypt_message_AES(message, key):
     #still got no clue. key is passed as an arg yet again
     decryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
     #plain_text is the decrypted message
     plain_text = decryption_suite.decrypt(message).decode('utf-8')
     return plain_text
+
+#RSA begins here
+def is_prime(a):
+    x = True
+    for i in (2, a):
+            while x:
+               if a%i == 0:
+                   x = False
+               else:
+                   x = True
+    if x:
+        print("prime")
+    else:
+        print("not prime")
+
+def generate_prime_numbers():
+    prime_number = number.getPrime(n_length)
+    return prime_number
