b06cdc7be5991c964f764f8042d84db191ae463f
ManiacalCactus <maniacalcactus@gmail.com>
2018-07-17 16:11:00 -0400

    Added documentation

--- protocol-source/card-code.c
+++ protocol-source/card-code.c
@@ -31,6 +31,7 @@ static uint8_t* readUART(){
 
 }
 
+//Reads the third 256 byte segment of data: salt
 static uint8_t* readSalt(uint8_t* buffer){
   uint8_t* result = (uint8_t*)malloc(SALT_SIZE*sizeof(uint8_t));
   for(int i = KEY_SIZE + SIGNATURE_SIZE; i < KEY_SIZE + SIGNATURE_SIZE + SALT_SIZE; i++){
@@ -39,6 +40,7 @@ static uint8_t* readSalt(uint8_t* buffer){
   return result;
 }
 
+//Reads the second 256 byte segment of data: signature
 static uint8_t* readSignature(uint8_t* buffer){
   uint8_t* result = (uint8_t*)malloc(SIGNATURE_SIZE*sizeof(uint8_t));
   for(int i = KEY_SIZE; i < KEY_SIZE + SIGNATURE_SIZE; i++){
@@ -47,6 +49,7 @@ static uint8_t* readSignature(uint8_t* buffer){
   return result;
 }
 
+//Reads the first 256 byte segment of data: contents
 static uint8_t* readData(uint8_t* buffer){
   uint8_t* result = (uint8_t*)malloc(KEY_SIZE*sizeof(uint8_t));
   for(int i = 0; i < KEY_SIZE; i++){
@@ -55,6 +58,7 @@ static uint8_t* readData(uint8_t* buffer){
   return result;
 }
 
+//Checks arrays against each other: for testing keys
 static int checkArrays(uint8_t array1, uint8_t array2){
   for(int i = 0; i < sizeof(array1)/sizeof(uint8_t); i++){
     if(array1[i] != array2[i]){
@@ -64,15 +68,19 @@ static int checkArrays(uint8_t array1, uint8_t array2){
   return 1;
 }
 
+
 int main() {
+  //Read memory and move to RAM
   uint8_t* privKey = readMemory(0);
   uint8_t* bankSig = readMemory(1);
   int* cardNum = (int*)readMemory(2);
 
+  //Send card number to ATM
   writeUART((uint8_t*)"___Sending card number___");
   writeUART((uint8_t*)cardNum);
   writeUART((uint8_t*)"___Sent card number___");
 
+  //Read checknum, decrypt and return with salt to ATM
   uint8_t* checkNumE = readUART();
   writeUART((uint8_t*)"___Received checkNum___");
   checkNumD = readData(rsaDecrypt(checkNumE, privKey));
@@ -80,7 +88,7 @@ int main() {
   uint8_t* salt = readSalt(rsaDecrypt(checkNumE, privKey));
   if(checkArrays(candBankSig, bankSig)){
     writeUART(checkNumD);
-    writeUART(salt)
+    writeUART(salt);
     writeUART((uint8_t*)"___Correct signature sent checknum___");
   }else{
     writeUART((uint8_t*)"___Incorrect signature did not send checknum___");
@@ -88,7 +96,7 @@ int main() {
 
 
 
-
+  //Read onion, decrypt, and send back inner layer with salt back to ATM
   uint8_t* onionE = readUART();
   writeUART((uint8_t*)"___Received onion___");
   onionD = readData(rsaDecrypt(onionE, privKey));
@@ -100,8 +108,7 @@ int main() {
     writeUART((uint8_t*)"___Correct signature sent onion___");
   }else{
     writeUART((uint8_t*)"___Incorrect signature did not send onion___");
+
   }
-
-
   return 0;
 }
