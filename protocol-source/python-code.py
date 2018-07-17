8b01de0e025908cd49ead3e1841d7b5168ea88d5
Allan Tenenbaum <tenenbaumallan@gmail.com>
2018-07-17 15:24:14 -0400

    need help making this work

--- protocol-source/python-code.py
+++ protocol-source/python-code.py
@@ -3,15 +3,26 @@ from Crypto import Random
 from Crypto.Cipher import AES
 import base64
 
-BLOCK_SIZE=32
+from Crypto.Cipher import AES
+# Encryption
+#encryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+#cipher_text = encryption_suite.encrypt("A really secret message. Not for prying eyes.")
 
-def encrypt(message, passphrase):
-    # passphrase MUST be 16, 24 or 32 bytes long, how can I do that ?
-    IV = Random.new().read(BLOCK_SIZE)
-    aes = AES.new(passphrase, AES.MODE_CFB, IV)
-    return base64.b64encode(aes.encrypt(message))
+# Decryption
+#decryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+#plain_text = decryption_suite.decrypt(cipher_text)
+encryption_key = "This is a key123"
+plain_text_message = "A really secret message. Not for prying eyes."
 
-def decrypt(encrypted, passphrase):
-    IV = Random.new().read(BLOCK_SIZE)
-    aes = AES.new(passphrase, AES.MODE_CFB, IV)
-    return aes.decrypt(base64.b64decode(encrypted))
+def encrypt(message, key):
+    encryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
+    cipher_text = encryption_suite.encrypt(message)
+    return cipher_text
+
+def decrypt(message, key):
+    ecryption_suite = AES.new(key, AES.MODE_CBC, 'This is an IV456')
+    plain_text = decryption_suite.decrypt(message)
+
+encoded_text = encrypt(plain_text_message, encryption_key)
+
+print(encoded_text)
