1e4d9bd9b15c038772e1bd09da65322fa94070aa
Allan Tenenbaum <tenenbaumallan@gmail.com>
2018-07-18 11:11:39 -0400

    generated RSA keys

--- protocol-source/ciphers.py
+++ protocol-source/ciphers.py
@@ -1,5 +1,8 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
 import os
 import base64
 import six
@@ -11,7 +14,7 @@ IV = 16 * '\x00'
 #here we define AES functions
 def create_aes_key():
     #this takes 16 random bytes to make our key
-    new_key = get_random_bytes(16)
+    new_key = get_random_bytes(32)
     return new_key
 
 def encrypt_aes(message, key):
@@ -24,4 +27,15 @@ def decrypt_aes(encrypted_message, key):
     decrypt_cipher = AES.new(key, AES.MODE_CBC, IV=IV)
     #the decode thing stops the result from being b'decrypted_message'
     plain_text = decrypt_cipher.decrypt(encrypted_message).decode("utf-8")
-    return plain_text`
+    return plain_text
+
+#here we define RSA functions
+def generate_rsa_keys():
+    secret_code = "Unguessable"
+    key = RSA.generate(2048)
+    encrypted_key = key.exportKey(passphrase=secret_code, pkcs=8)
+    file_out = open("rsa_key.bin", "wb")
+    file_out.write(encrypted_key)
+    print(key.publickey().exportKey())
+#any test code goes here
+generate_rsa_keys()
