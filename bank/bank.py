5048b36f095b2a97abc180226b476840efd6a39f
ManiacalCactus <maniacalcactus@gmail.com>
2018-07-21 17:29:15 -0400

    Broke stuff :(

--- bank/bank.py
+++ bank/bank.py
@@ -56,49 +56,26 @@ class Bank(object):
                 self.atm.write(self.ERROR)
 
     def withdraw(self, atm_id, card_id, amount):
-        try:
-            amount = int(amount)
-            atm_id = str(atm_id)
-            card_id = str(card_id)
-        except ValueError:
-            self.atm.write(self.ERROR)
-            log("Bad value sent")
-            return
-
-        atm = self.db.get_atm(atm_id)
-        if atm is None:
-            self.atm.write(self.ERROR)
-            log("Bad ATM ID")
-            return
-
-        num_bills = self.db.get_atm_num_bills(atm_id)
-        if num_bills is None:
-            self.atm.write(self.ERROR)
-            log("Bad ATM ID")
-            return
-
-        if num_bills < amount:
+        if self.db.get_atm(atm_id) is None:
             self.atm.write(self.BAD)
-            log("Insufficient funds in ATM")
+            log("Invalid ATM ID")
             return
 
-        balance = self.db.get_balance(card_id)
-        if balance is None:
+        balance = 0
+        onion = self.db.get_onion(str(card_id))
+        if onion is None:
             self.atm.write(self.BAD)
             log("Bad card ID")
-            return
-
-        final_amount = balance - amount
-        if final_amount >= 0:
-            self.db.set_balance(card_id, final_amount)
-            self.db.set_atm_num_bills(atm_id, num_bills - amount)
-            log("Valid withdrawal")
-            pkt = struct.pack(">36s36sI", atm_id, card_id, amount)
-            self.atm.write(self.GOOD)
-            self.atm.write(pkt)
         else:
-            self.atm.write(self.BAD)
-            log("Insufficient funds in account")
+            log("Valid balance check")
+            self.encSend(onion)
+            innerLayer = self.encRead(ONION_SIZE)
+            balance = ciphers.decrypt_rsa(innerLayer, self.db.get_outer_onion_public_key)
+            self.encSend(self.GOOD)
+
+        if amount > balance:
+            #todo finish!!!!
+            self.atm.write("sent stuff")
 
     def check_balance(self, atm_id, card_id): #finished
         if self.db.get_atm(atm_id) is None:
@@ -114,7 +91,7 @@ class Bank(object):
             log("Valid balance check")
             self.encSend(onion)
             innerLayer = self.encRead(ONION_SIZE)
-            ciphers.decrypt_rsa(innerLayer, self.db.get_outer_onion_public_key)
+            balance = ciphers.decrypt_rsa(innerLayer, self.db.get_outer_onion_public_key)
             self.encSend(self.GOOD)
 
 
