750525b7207235e5c91863a1e243bf52570e5faa
Ceiran <33044342+Ceiran@users.noreply.github.com>
2018-07-27 12:39:08 -0400

    Some documentation.

--- atm/ciphers.py
+++ atm/ciphers.py
@@ -10,22 +10,22 @@ import six
 from Crypto.Cipher import PKCS1_OAEP
 import secrets
 
-#here we define initial variables
+# here we define initial variables
 n_length = 2048
 our_message = "testing here please"
 
-#here we define AES functions
+# here we define AES functions
 def pad(unpadded_message, pad_length):
     padded_message = unpadded_message + (((pad_length - len(unpadded_message)) % pad_length * '!'))
     return padded_message
 
 def create_aes_key():
-    #this takes 32 random bytes to make our key
+    # this takes 32 random bytes to make our key
     new_key = get_random_bytes(32)
     return new_key
 
 def encrypt_aes(message, key):
-    #key has to be 16 bytes long
+    # key has to be 16 bytes long
     message = pad(message)
     global IV
     IV = get_random_bytes(16)
@@ -35,19 +35,17 @@ def encrypt_aes(message, key):
 
 def decrypt_aes(message, key):
     decrypt_cipher = AES.new(key, AES.MODE_CBC, IV=IV)
-    #the decode thing stops the result from being b'decrypted_message'
+    # the decode thing stops the result from being b'decrypted_message'
     plain_text = decrypt_cipher.decrypt(message).decode("utf-8")
     return plain_text
 
-#here we define RSA functions
+# generates a prime number
 def generate_prime_number():
     generated_number = number.getPrime(n_length)
     return generated_number
 
-#not sure if these imports are more or less correct than the ones above for RSA.
-#What version should we use for RSA..?
 
-#generates rsa key
+# generates rsa key
 def generate_key():
     #using generate() to generate key
     #first parameter can be any number that is a multiple of 256 and greater than 1024
@@ -56,9 +54,9 @@ def generate_key():
     public = private.publickey()
     return private, public
 
-#rsa encryption
+# RSA encryption
 def encrypt_rsa(message, pub_key):
-    #applies RSA Padding
+    # applies RSA Padding
     rsa_pub_cipher = PKCS1_OAEP.new(pub_key)
     encrypted_rsa = rsa_pub_cipher.encrypt(message)
     return encrypted_rsa
@@ -68,15 +66,16 @@ def encrypt_rsa(message, pub_key):
     return encrypted_rsa
     '''
 
-#rsa decryption
+# RSA decryption
 def decrypt_rsa(encrypted_rsa, priv_key):
-    #applies RSA Padding
+    # applies RSA Padding for more security
     rsa_priv_cipher = PKCS1_OAEP.new(priv_key)
     decrypted_rsa = rsa_priv_cipher.decrypt(encrypted_rsa).decode("utf-8")
     return decrypted_rsa
     decrypted_rsa = priv_key.decrypt(encrypted_rsa).decode("utf-8")
     return decrypted_rsa
-    
+
+# generates a new salt every time it is run
 def hash_message(message):
     salt = bcrypt.gensalt()
     message = str(message)
@@ -85,7 +84,7 @@ def hash_message(message):
     message = message.encode("utf-8")
     return(hashlib.sha3_256(message).hexdigest())
 
-def diffie_bank():
+def diffie_atm():
     mod, bas = "getting the mod and base"
     #insert write (mod, bas) to atm
     b = secrets.randbelow(9999)
