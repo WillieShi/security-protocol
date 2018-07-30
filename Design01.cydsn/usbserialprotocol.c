2070ff6043c02030e9269fb2eba4f5f5a35ff745
ManiacalCactus <maniacalcactus@gmail.com>
2018-07-30 09:06:36 -0400

    added get valid bytes method

--- Design01.cydsn/usbserialprotocol.c
+++ Design01.cydsn/usbserialprotocol.c
@@ -29,17 +29,28 @@ uint8 getValidByte()
     return retval;
 }
 
+uint8_t* getValidBytes(int size)
+{
+    uint8_t retval[size];
+    for(int i = 0; i < size; i++){
+      while(UART_SpiUartGetRxBufferSize() < 1); // wait for byte
+      retval[i] = UART_UartGetByte();
+    }
+    return retval;
+}
+
+
 
 int pushMessage(const uint8 data[], uint8 size)
 {
     int i;
 
     UART_UartPutChar(size);
-    
+
     for (i = 0; i < size; i++) {
-        UART_UartPutChar(data[i]);   
+        UART_UartPutChar(data[i]);
     }
-    
+
     return RECV_OK;
 }
 
@@ -47,17 +58,17 @@ int pushMessage(const uint8 data[], uint8 size)
 uint8 pullMessage(uint8 data[])
 {
     int i, len;
-    
+
     len = getValidByte();
-    
+
     for (i = 0; i < len; i++) {
-        data[i] = getValidByte();   
+        data[i] = getValidByte();
     }
-    
+
    return len;
 }
 
-/* 
+/*
  * generic PSoC synchronization protocol:
  *
  * 1) ATM -> "READY" -> PSoC
@@ -66,29 +77,29 @@ uint8 pullMessage(uint8 data[])
  * 3) ATM -> "GO" -> PSoC
  * 4) if bad: goto 1)
  */
-void syncConnection(int prov) 
+void syncConnection(int prov)
 {
     uint8 message[32];
-    
+
     // marco-polo with bank until connection is in sync
     do {
         pullMessage(message);                               // 1)
-        
+
         if (strcmp((char*)message, RDY_MSG_RECV)) {
             pushMessage(message, strlen((char*)message));   // 2) bad
             strcpy((char*)message, RDY_BAD);
         } else if (prov) {
-            pushMessage((uint8*)RDY_MSG_PROV, 
+            pushMessage((uint8*)RDY_MSG_PROV,
                         strlen(RDY_MSG_PROV));              // 2) good prov
-            
+
             pullMessage(message);                           // 3)
         } else {
-            pushMessage((uint8*)RDY_MSG_NORM, 
+            pushMessage((uint8*)RDY_MSG_NORM,
                         strlen(RDY_MSG_NORM));              // 2) good norm
-            
+
             pullMessage(message);                           // 3
         }
-        
+
     } while (strcmp((char*)message, GO_MSG));               // 4)
 }
 
