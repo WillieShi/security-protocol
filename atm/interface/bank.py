8f65f87f6ffc9f35cadf2bf9e89bb9442b77787e
Ceiran <33044342+Ceiran@users.noreply.github.com>
2018-08-03 08:52:00 -0400

    Some more documentation

--- atm/interface/bank.py
+++ atm/interface/bank.py
@@ -40,8 +40,10 @@ class Bank:
     def diffie_atm(self):
         # Receives modulus and base from bank.
         transaction_id, mod, base = struct.unpack(">32s256s256s", self.default_read(544))
+        # converts mod and base from bytes to int using process()
         mod = process(mod)
         base = process(base)
+        # random.randint() is a pseudorandom num generator that returns a value N such that a <= N <= b
         secret_number_a = randint(0, 9999)
         side_atm = (base**secret_number_a) % mod
         # Receives bank's half of diffie hellman from bank to compute final value.
@@ -50,7 +52,8 @@ class Bank:
         # Sends ATM's half of diffie hellman to bank.
         self.default_write(struct.pack("32s256s", format("dif_side_atm"), format(side_atm, 256)))
         # uptime_key_atm is the final ATM-side agreed value for diffie hellman
-# #################################RECIEVE THE IV FROMT THE BANK
+        # RECIEVE THE IV FROMT THE BANK
+        # Fix
         self.bank_key, self.bank_IV = (side_bank**secret_number_a) % mod
 
     def _vp(self, msg, stream=logging.info):
@@ -63,19 +66,23 @@ class Bank:
         if self.verbose:
             stream("card: " + msg)
 
+    # Sends to bank the card-encrypted balance (only card and bank have AES key), and the atm-encrypted balance (only atm and bank have AES key)
     def read_verify_or_withdraw(self):
         card_encrypted_balance, IV, atm_encrypted_balance = struct.pack(">16s16s16s", self.default_read(48))
         return card_encrypted_balance, IV, ciphers.decrypt_aes(atm_encrypted_balance, self.bank_key, self.bank_IV)
 
+    # Encrypts information needed to verify user with atm-bank-only AES and sends to bank
     def write_verify(self, encrypted_hashed_passkey, card_id, pin):
         pkt = struct.pack(">32s32s16s", encrypted_hashed_passkey, ciphers.encrypt_aes(ciphers.hash_message(card_id+pin), self.bank_key, self.bank_IV), ciphers.encrypt_aes(card_id, self.bank_key, self.bank_IV))
         self.default_write(pkt)
 
+    # Sends user's requested withdraw amount from atm to bank after encrypting
     def write_withdraw(self, withdraw_amount):
         pkt = struct.pack(">16s", ciphers.encrypt_aes(withdraw_amount, self.bank_key, self.bank_IV))
         self.default_write(pkt)
         return True
 
+    # Begins provision process and sends important data to bank
     def provision_update(self, aes_key, IV, card_num, hashed_passkey, hashed_data):
         pkt = struct.pack(">32s16s16s32s32s", aes_key, IV, card_num, hashed_passkey, hashed_data)
         # total length is not 1088 bytes
@@ -85,6 +92,7 @@ class Bank:
         self.ser.write("f")
 
 
+# Used to reformat inputs to bytes, which can then be packed using struct
 def format(value, size=256):
     if type(value) is str:
         return bytes(value, "utf-8")
@@ -92,5 +100,6 @@ def format(value, size=256):
         return (value).to_bytes(size, byteorder='little')
 
 
+# Converts bytes back into int, only works on int
 def process(value):
     return int.from_bytes(value, byteorder="little")
