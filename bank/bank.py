9ce66c5cc9beeba07bb9ee2cc3c1de174636b17e
Ceiran <33044342+Ceiran@users.noreply.github.com>
2018-08-02 23:09:06 -0400

    Bank.py in bank documented

--- bank/bank.py
+++ bank/bank.py
@@ -50,6 +50,8 @@ class Bank(object):
         return generated_number
 
     # Used to measure sizes of a given value in bytes, for debugging
+    # code in else statement is py2.7 "version" of py3's to_bytes()
+    # Uses different method to measure byte size when input is a string bc to_bytes() only works with int
     def bytesize(self, value):
         if type(value) is str:
             return len(bytes(value, "utf-8"))
@@ -71,6 +73,7 @@ class Bank(object):
         mod, base = self.diffie_hellman()
         #  Sends modulus and base to ATM
         self.default_write(struct.pack(">32s256s256s", format("dif_mod_base"), format(mod, 256), format(base, 256)))
+        # random.randint() is a pseudorandom num generator that returns a value N such that a <= N <= b
         secret_number_b = random.randint(1, 9999)
         side_bank = (base**secret_number_b) % mod
         # Sends bank's half of diffie hellman to ATM.
@@ -81,7 +84,7 @@ class Bank(object):
         self.atm_key, self.atm_IV = (side_atm**secret_number_b) % mod, ciphers.generate_salt(16)
 
     # Links commands in ATM-Bank interface to functions in the bank
-    # Three-letter codes link interface commands to bank functions.
+    # Three-letter codes link interface commands to bank functions (see top of this file for key on three-letter codes)
     # Initializes AES Key first upon power cycle with diffie_bank()
     def start(self):
         self.diffie_bank()
@@ -95,6 +98,8 @@ class Bank(object):
             elif command != "":
                 self.atm.write(self.ERROR)
 
+    # Reads hashed data that was sent by card to verify the card/account using default.read(), and then decrypts the AES on data
+    # Gets user's balance using the decrypted data
     def verify(self):
         encrypted_hashed_passkey, encrypted_hashed_data, encrypted_card_id = struct.unpack(">32s32s16s", self.default_read(80))
         # encrypted_hashed_passkey = process(encrypted_hashed_passkey)
@@ -104,7 +109,7 @@ class Bank(object):
         hashed_data = ciphers.decrypt_aes(encrypted_hashed_data, self.atm_key)
 
         balance = ciphers.decrypt_aes(self.db.get_encrypted_balance(), hashed_data)
-
+        # If hashed_passkey corresponds to bank's record, AES channel between the bank and card will be established to encrypt the data with
         if hashed_passkey == self.db.get_hashed_passkey(card_id):
             newIV = ciphers.generate_salt(16)
             self.default_write(struct.pack(">16s16s16s", ciphers.encrypt_aes(balance, self.db.get_aes_key(card_id), self.db.get_iv(card_id)), newIV, ciphers.encrypt_aes(balance, self.atm_key, self.atm_IV)))
@@ -114,11 +119,15 @@ class Bank(object):
         else:
             self.default_write("Nice try kid, papa john taught me all the tricks")
 
+    # Reads requested encrypted withdraw amount that came from card
+    # Decrypts AES to read the withdraw_amount
     def withdraw(self, balance, card_id, hashed_data):
         encrypted_withdraw_amount = struct.unpack(">16s", self.default_read(16))
         withdraw_amount = ciphers.decrypt_aes(encrypted_withdraw_amount, self.db.get_aes_key(card_id), self.db.get_iv(card_id))
-
-        if balance > withdraw_amount:
+    # If the user has enough money to withdraw the requested amount, the bank will proceed with calculating the remaining balance
+    # Will send the user the new balance after encrypting it with AES
+    # If the user does not have enough money, an error message will be sent
+        if balance >= withdraw_amount:
             new_balance = balance - withdraw_amount
             newIV = ciphers.generate_salt(16)
             self.default_write(struct.pack(">16s16s16s", ciphers.encrypt_aes(new_balance, self.db.get_aes_key(card_id), self.db.get_iv(card_id)), newIV, ciphers.encrypt_aes(new_balance, self.atm_key, self.atm_IV)))
@@ -129,6 +138,7 @@ class Bank(object):
             self.default_write("You're broke ponyboy")
 
 
+# Generates new AES key using the old key and the current balance hashed together
 def gen_new_key(old_key, balance):
     return ciphers.hash_message(format(old_key) + format(balance))
 
@@ -140,6 +150,7 @@ def parse_args():
     return parser.parse_args()
 
 
+# Used to reformat inputs to bytes, which can then be packed using struct
 def format(value, size=256):
     if type(value) is str:
         return bytes(value, "utf-8")
@@ -147,6 +158,7 @@ def format(value, size=256):
         return (value).to_bytes(size, byteorder='little')
 
 
+# Converts bytes back into int, only works on int
 def process(value):
     return int.from_bytes(value, byteorder="little")
 
